9815f300cfc4a1dd9a35bf946fdd88d78bf916af
johntu <johntu@kth.se>
2018-12-20 23:36:41 +0100

    Merged with root finder version

--- cgpy/cgp.py
+++ cgpy/cgp.py
@@ -173,7 +173,6 @@ def convert_rec(op_table, gene, variable_names, nr_of_nodes, total_dims, nr_of_p
 	op = op_table[gene[3 * (current_node_nr-total_dims) + 0]]
 	nr_of_vars = total_dims-nr_of_parameters
 
-
 	if op.is_binary:
 		# The string is given by (str1)#(str2) if the operation is binary. 
 		# The #-sign is replaced by the sign of the operation. str1 and str2 
@@ -219,6 +218,7 @@ def convert_rec(op_table, gene, variable_names, nr_of_nodes, total_dims, nr_of_p
 				middle_str = variable_names[gene[3 * (current_node_nr-total_dims) + 1]]
 			else:
 				middle_str = str(parameters[gene[3 * (current_node_nr-total_dims) + 1] - nr_of_vars])
+
 		else:
 			#... if it isn't, we need a recursive function call to calc it.
 			middle_str = convert_rec(op_table, gene, variable_names, nr_of_nodes, total_dims, nr_of_parameters, gene[3 * (current_node_nr-total_dims) + 1], parameters=parameters)
@@ -229,7 +229,6 @@ def convert_rec(op_table, gene, variable_names, nr_of_nodes, total_dims, nr_of_p
 			return "("+middle_str+")^{2}"
 			
 		return op.str+"("+middle_str+")"
-
 
 class CGP():
 	"""
@@ -277,6 +276,7 @@ class CGP():
 		# Nr of nodes excluding the output node
 		nr_of_nodes = int((len(self.gene)-1)/3)+self.dims+self.nr_of_parameters
 		self.has_setup_used_nodes = True
+
 		class CGPNode():
 			"""Temporary node object"""
 			def __init__(self, upstream1, upstream2, is_used=False):
@@ -334,6 +334,13 @@ class CGP():
 		"""
 		Evaluates the function at point X using the parameters
 		in parameters.
+
+		If derivative is true, then it takes the derivative in the point as well.
+		It return the tuple (function_val, derivative_val) in that case.
+
+		der_dir is the direction of the derivative. Which means that der_dir=0 => derivative
+		with respect to X[0], and so on. If der_dir >= len(X), then we will start deriving 
+		with respect to the parameters
 		"""
 		assert(self.nr_of_parameters == len(parameters))
 		if self.dims != len(X):
@@ -450,6 +457,92 @@ class CGP():
 		# Create the new CGP object
 		new_cgp = CGP(self.dims, self.op_table, new_gene, nr_of_parameters=self.nr_of_parameters, fast_setup=self.setup_used_nodes_list==False)
 		return new_cgp
+
+	def print_function(self, parameters=[], var_names=None):
+		print(self.convert2str(parameters=parameters, var_names=var_names))
+
+	def merge_lists(self, x1, x2):
+		out = []
+
+		for x in x1:
+			if x not in out:
+				out.append(x)
+		for x in x2:
+			if x not in out:
+				out.append(x)
+		return out
+
+	def which_variables_and_parameters_are_used(self):
+		# Combined dimensionality of the variables and parameters.
+		total_dims = self.dims + self.nr_of_parameters
+		n = int((len(self.gene)-1)/3) + total_dims
+		node_depends_on_which_nodes = [[] for _ in range(n)]
+
+		gene_counter = 0
+
+		for i in range(total_dims):
+			if i < self.dims:
+				node_depends_on_which_nodes[i].append(i)
+			else:
+				node_depends_on_which_nodes[i].append(i)
+
+		for node_nr in range(total_dims, n):
+			op = self.op_table[self.gene[gene_counter]]
+			gene_counter += 1
+
+			# The node has 2 connections if the operation is binary,
+			# and 1 connection otherwise.
+			if op.is_binary:
+				x1 = node_depends_on_which_nodes[self.gene[gene_counter]]
+				gene_counter += 1
+				x2 = node_depends_on_which_nodes[self.gene[gene_counter]]
+				gene_counter += 1
+
+				x = self.merge_lists(x1, x2)
+			else:
+				x = list(node_depends_on_which_nodes[self.gene[gene_counter]])
+				gene_counter += 1
+				gene_counter += 1
+
+			node_depends_on_which_nodes[node_nr] = x
+
+		assert gene_counter == len(self.gene)-1
+		return sorted(node_depends_on_which_nodes[self.gene[gene_counter]])
+
+	def which_parameters_are_used(self):
+		# TODO: This shouldn't be re-calculated every time.
+		var_and_par = self.which_variables_and_parameters_are_used()
+
+		is_parameter_used = [False for _ in range(self.nr_of_parameters)]
+
+		for x in var_and_par:
+			# Ignore the variables, and only focus on the parameters.
+			if x >= self.dims:
+				par_nr = x - self.dims
+				is_parameter_used[par_nr] = True
+		return is_parameter_used
+
+def how_many_genes_exists(dims, nr_of_nodes, ignore_id=True):
+	from operation_table import op_table
+
+	nr_of_ops = len(op_table)
+	if ignore_id:
+		if 'id' in [op.op_name for op in op_table]:
+			nr_of_ops -= 1
+
+	nr_of_binary_ops = sum([1 if op.is_binary else 0 for op in op_table])
+	nr_of_unary_ops = nr_of_ops - nr_of_binary_ops
+
+	counter = 1
+	for i in range(nr_of_nodes):
+		prev_nodes = dims + i
+
+		counter *= nr_of_binary_ops*prev_nodes*prev_nodes + nr_of_unary_ops*prev_nodes
+
+	# And lastly the choice of output node
+	counter *= nr_of_nodes+dims
+
+	return counter
 
 if __name__ == '__main__':
 	# This are all operations that the CGP is allowed to use. These are not set in stone.
