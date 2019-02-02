29e16d1a9f44d14ff6075bfa354fb4fee00d0f1b
Viktor Wase <viktorwase@gmail.com>
2019-02-02 11:53:30 +0100

    Update version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cgpy",
-    version="0.0.1",
+    version="0.0.2",
     author="Viktor Wase",
     author_email="viktorwase@gmail.com",
     description="A Python library for Cartesian genetic programming.",
